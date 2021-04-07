04bcdb5d1aada16946939ce2d716b01f1c607e12
preethi_hiremath <preethivhiremath.vh@gmail.com>
2021-04-07 08:18:38 +0530

    chat-app react

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '6a278f47-5489-4540-b3a2-573a710eae66';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+export default App;
